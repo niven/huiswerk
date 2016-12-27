2087ca31308dda9e86027040e8422c1e78b2e338
Chris Veenboer <chris.veenboer@gmail.com>
2016-12-27 21:57:53 +0100

    Moved shuffle() for arrays to generic util file

--- modules/fractions/util.js
+++ modules/fractions/util.js
@@ -1,24 +1,3 @@
-// Fisher-Yates
-function shuffle( array ) {
-	
-    var counter = array.length, temp, index;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
-}
 
 function rational_compare( fraction_a, fraction_b ) {
 	
